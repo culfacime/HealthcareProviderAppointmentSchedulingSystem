95bf3fb43132548f9c39e20f6498e9059974203a
Muhammed Emin ÇULFACI <mculfaci@gmail.com>
2023-07-13 23:42:38 +0300

    hangfire integration for cronjobs

--- Healthcare.API/Program.cs
+++ Healthcare.API/Program.cs
@@ -1,12 +1,21 @@
 using DotNetEnv;
+using Hangfire;
 using Healthcare.API.Extensions;
 using Healthcare.Core.DB;
+using Healthcare.Core.Repositories;
+using Healthcare.Core.Services;
+using Healthcare.Core.UnitOfWorks;
+using Healthcare.Repository.Repository;
+using Healthcare.Repository.UnitOfWorks;
+using Healthcare.Service.Services;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +24,18 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllersWithViews()
                 .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
+
+builder.Services.AddHangfire(configuration => configuration
+        .UseSqlServerStorage(Env.GetString("DB_CONNECTION"), new Hangfire.SqlServer.SqlServerStorageOptions
+        {
+             PrepareSchemaIfNecessary = true
+        }));
+
+builder.Services.AddHangfireServer();
 
 EnvExtensions.LoadEnv();
 
@@ -31,6 +52,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHangfireDashboard();
 
 app.UseHttpsRedirection();
 
